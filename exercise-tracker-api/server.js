47ca5457d530daa074cf28beff80a2ca548f4c56
sdthaker <71615163+sdthaker@users.noreply.github.com>
2021-07-09 10:18:37 -0400

    Update server.js

--- exercise-tracker-api/server.js
+++ exercise-tracker-api/server.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb+srv://sdthaker:Abcd123456@cluster1.lxvkb.mongodb.net/exerciseTracker?retryWrites=true&w=majority');
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true});
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
